e93096ae783cf031adef6a6160f2088fd89b62af
tina.cai <tina.cai@mart.blue>
2020-11-24 11:49:10 +0800

    init

---
+++ precache-manifest.9fe576694b9ceb6454f829daac483355.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "015160e63fbd04526fed",
+    "url": "/css/app.37e87b57.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "bd2d224e7883579c1e018900cf42b767",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e6da5ba8a2092449893",
+    "url": "/js/about.8cb1d020.js"
+  },
+  {
+    "revision": "015160e63fbd04526fed",
+    "url": "/js/app.2f513c3b.js"
+  },
+  {
+    "revision": "f624121200eeee95d209",
+    "url": "/js/chunk-vendors.b918eebf.js"
+  },
+  {
+    "revision": "fe8837b4b848f95e9acaea778b167ab5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
